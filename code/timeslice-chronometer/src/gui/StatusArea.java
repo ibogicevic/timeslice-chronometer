0153444d10af94f73e68148876695ff66e870145
Ivan Bogicevic <bogicevic@informatik.uni-stuttgart.de>
2017-01-26 11:28:14 +0100

    added infotext at the buttom

--- code/timeslice-chronometer/src/gui/StatusArea.java
+++ code/timeslice-chronometer/src/gui/StatusArea.java
@@ -1,11 +1,30 @@
 package gui;
 
+import javafx.geometry.Insets;
 import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
 
 /**
  * StatusArea at the bottom of the main window
  * @author ivan
  */
-public class StatusArea extends Label {
-
+public class StatusArea extends GridPane {
+	
+	// info text
+	final private String INFOTEXT = ""
+			+ "A..Z: switch to associated slice timer, increase counter\n"
+			+ "SPACE: pause measurement\n"
+			+ "BACKSPACE: restart measurement\n"
+			+ "ENTER: save data as export.csv\n"
+			+ "ESCAPE: exit application\n";
+	
+	public StatusArea() {
+		// pane styling
+		this.setPadding(new Insets(10, 10, 10, 10));
+		this.setHgap(5);
+		this.setVgap(5);
+		// add info text
+		this.add(new Label(INFOTEXT), 1, 1);
+	}
+	
 }
