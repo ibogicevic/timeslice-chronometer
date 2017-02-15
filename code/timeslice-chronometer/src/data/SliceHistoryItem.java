fd8ab37602926ca57980a104249beebd3f320d1b
Ivan Bogicevic <bogicevic@informatik.uni-stuttgart.de>
2017-02-15 17:08:38 +0100

    code formatting (minor)

--- code/timeslice-chronometer/src/data/SliceHistoryItem.java
+++ code/timeslice-chronometer/src/data/SliceHistoryItem.java
@@ -3,11 +3,10 @@ package data;
 /**
  * A single step in a slice history, i.e. the slice letter with its timestamp.
  * @author ivan
- *
  */
 public class SliceHistoryItem {
 	/** Letter (key) of the slice */
 	public String triggerKey;
-	/**	Number of seconds from begin of measurements to slice start */
+	/** Number of seconds from begin of measurements to slice start */
 	public String timeStamp;
 }
